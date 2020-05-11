0c506e39bd6c5a1bb2aa65d1a22e7e02772be57c
Kyu Hyun Kim <kyuhyunkim07@gmail.com>
2020-05-11 20:53:53 +0900

    Second commit

--- R/rq_est.R
+++ R/rq_est.R
@@ -1,4 +1,4 @@
-#' Quantile regression estimator using induced smoothing approach
+#' Quantile regression estimator (Kim et al)
 #'
 #' ismb_est function will calculate qunatile regression parameters "beta" and standard error of "beta" using induced smoothing approach.
 #'
@@ -8,26 +8,48 @@
 #' @param D A vector of subjects' censored indicator (1 = not_censored data, 0 = censored data)
 #' @param t_0 Followup time or base time adjustment (minimum t_0 is 0, and it means no base time adjustment)
 #' @param Q Quantile that user want to estimate (scale : 0 ~ 1)
-#' @param W A vector of weights
-#' @param ne Number of eta generation when estimate standard error of estimator
 #' @return A nc by 2 matrix (First column is beta and second column is standard error of betas)
 #' @examples
 #'     covar = as.matrix(data[,1:12])
 #'     ismb_est(data$survTime, 12, covar, data$event, 2, 0.5 ,100)
 #' @export
-ismb_est = function(Z, nc, covariate, D, t_0, Q, W, ne){
+rq_est = function(Z, nc, covariate, D, t_0, Q){
   n = length(Z)
-  data = matrix(NA, n, nc+5)
+  data = matrix(NA, n, nc+6)
   data[,1] = Z
   data[,2] = log(Z-t_0)
   data[,3] = as.numeric(Z>t_0)
   data[,4:(nc+3)] = covariate
   data[,(nc+4)] = D
-  data[is.nan(data[,2]),2]=-10
+  data[is.nan(data[,2]),2]=NA
   data = as.data.frame(data)
-  covar_names = paste("covariate",1:nc,sep="")
-  colnames(data)[4:(nc+3)] = covar_names
-  colnames(data)[(nc+4):(nc+5)] = c("delta","Weight")
+
+  # Weight
+  # Kaplan-Meier estimator for censoring
+  # survfit weight
+  # fit = survfit(Surv(data[,1],(1-data[,(nc+4)])) ~ 1)
+  # for (i in 1:length(fit$surv))
+  # {
+  #   data[data[,1]==fit$time[i],(nc+5)] = fit$surv[i]
+  # }
+  # WKM weight
+  fit = WKM(data[,1],  1-data[,(nc+4)], zc = rep(1,n), w = rep(1,n))
+  for (i in 1:length(fit$surv))
+  {
+    data[data[,1]==fit$times[i],(nc+5)] = fit$surv[i]
+  }
+  data[,(nc+6)] = data[,(nc+4)]/data[,(nc+5)]
+  m = nrow(data)
+  if (data[m,(nc+4)]==0){
+    data[m,(nc+6)]=0
+  } else {
+    max(data[1:(m-1),(nc+6)])
+  }
+  colnames(data)[1:3]=c("Z", "log(Z-t_0)","I[T>=C]")
+  for (j in 4:(nc+3)){
+    colnames(data)[j]="covariate"
+  }
+  colnames(data)[(nc+4):(nc+6)] = c("delta","G_KM","Weight")
 
   # Objective equation
   objectF = function(beta){
