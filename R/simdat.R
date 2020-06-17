4bdab4c666585e0f6c7aad231902c224b28a6c95
Steven Chiou <syhan.chiou@gmail.com>
2020-06-17 13:58:47 -0500

    fix b0

--- R/simdat.R
+++ R/simdat.R
@@ -3,8 +3,6 @@
 ## Summarized from Simulation\ data\ generator.R and ndata2.R
 ## #################################################################
 
-n <- 10
-
 #' External variables; t0 = 1, 3, 5, 7
 #' @param n is sample size
 #' @param cp is censoring rate
@@ -27,30 +25,42 @@ n <- 10
 #' Z.1 -- Z.4 are the residual times at t0 = 1, 2, 3, 4
 #' d.1 -- d.4 are the censoring indicators for the corresponding Z's
 
-n <- 100
-cen <- .2
-sce <- 2
-q <- .25
-
 datGen <- function(n, cp, sce, q) {
     k <- 2
     X <- model.matrix(~ sample(0:1, n, TRUE))
-    ## Calculate b
-    b0 <- log(5)
-    t0 <- 0:3
     if (sce == 1) b <- matrix(c(1.609438, 1.410748, 1.219403, 1.040613 , 0, 0, 0, 0), 4)
     else b <- matrix(c(1.609438, 1.410748, 1.219403, 1.040613,
                        0.6931472, 0.7974189, 0.9070615, 1.0174711), 4)
     u <- runif(n)
-    if (cp == Inf) cen <- Inf
-    if (cp == .1) cen <- t(matrix(runif(4 * n, 0, c(123.69, 70.39, 64.86, 61.17)), 4))
-    if (cp == .3) cen <- t(matrix(runif(4 * n, 0, c(41.27, 24.35, 23.34, 22.53)), 4))
-    if (cp == .5) cen <- t(matrix(runif(4 * n, 0, c(23.55, 14.07, 13.62, 13.43)), 4))
-    if (cp == .7) cen <- t(matrix(runif(4 * n, 0, c(14.2, 8.49, 8.36, 8.5)), 4))
-    Y <- (-log(u))^(1 / k) / ((-log(q))^(1 / k) / exp(X %*% t(b)))
-    Z <- pmin(Y, cen)
+    if (cp == 0) cen <- Inf
+    if (sce == 1) {
+        if (cp == .1) cen <- t(matrix(runif(4 * n, 0, c(123.69, 70.39, 64.86, 61.17)), 4))
+        if (cp == .3) cen <- t(matrix(runif(4 * n, 0, c(41.27, 24.35, 23.34, 22.53)), 4))
+        if (cp == .5) cen <- t(matrix(runif(4 * n, 0, c(23.55, 14.07, 13.62, 13.43)), 4))
+        if (cp == .7) cen <- t(matrix(runif(4 * n, 0, c(14.2, 8.49, 8.36, 8.5)), 4))
+    }
+    if (sce == 2) {
+        if (cp == .1) cen <- t(matrix(runif(4 * n, 0, c(78.11, 70.39, 64.86, 61.17)), 4))
+        if (cp == .3) cen <- t(matrix(runif(4 * n, 0, c(26.36, 24.35, 23.34, 22.53)), 4))
+        if (cp == .5) cen <- t(matrix(runif(4 * n, 0, c(15.08, 14.07, 13.62, 13.43)), 4))
+        if (cp == .7) cen <- t(matrix(runif(4 * n, 0, c(9.09, 8.49, 8.36, 8.5)), 4))
+    }
+    ## Y <- (-log(u))^(1 / k) / ((-log(q))^(1 / k) / exp(b[1,] %*% apply(X, 1, cumsum)))
+    Y <- (-log(u))^(1 / k) / ((-log(q))^(1 / k) / exp(colSums(b[1,] * apply(X, 1, cumsum))))
+    Z <- matrix(pmin(Y, cen), n)
     delta <- 1 * (Y <= cen)
+    delta[t(t(Z) <= 0:3)] <- NA
+    Z[t(t(Z) <= 0:3)] <- NA
     colnames(X) <- 1:2
     data.frame(Z = Z, X = X, d = delta)
 }
 
+summary(datGen(1e5, .1, 1, .5)[,7:10])
+summary(datGen(1e5, .3, 1, .5)[,7:10])
+summary(datGen(1e5, .5, 1, .5)[,7:10])
+summary(datGen(1e5, .7, 1, .5)[,7:10])
+
+summary(datGen(1e5, .1, 2, .5)[,7:10])
+summary(datGen(1e5, .3, 2, .5)[,7:10])
+summary(datGen(1e5, .5, 2, .5)[,7:10])
+summary(datGen(1e5, .7, 2, .5)[,7:10])
