7c3a1ec9dbf64d2ee8df337297725c73e4bb2dcd
kyuhyunkim07@gmail.com <35562513+Kyuhyun07@users.noreply.github.com>
2021-11-01 13:04:34 +0900

    Changed example and correct errors

--- R/qrismb.R
+++ R/qrismb.R
@@ -28,8 +28,9 @@
 #' @example inst/examples/ex_qrismb.R
 qrismb <- function(Z, covariate, D, t_0 = 0, Q = 0.5, ne = 100, init="rq", method="smooth"){
     nc <- ncol(covariate)
-    if(nc < 1)
+    if(nc < 1){
         stop("Use at least one covariate")
+    }
     if(sum(D!=0&D!=1)>=1) {
         stop("Delta must consist of only 0 and 1")
     }
@@ -47,29 +48,39 @@ qrismb <- function(Z, covariate, D, t_0 = 0, Q = 0.5, ne = 100, init="rq", metho
     }
 
     n = length(Z)
-    data <- matrix(NA, n, nc+5)
+    data <- matrix(NA, n, nc+4)
     data[,1] <- Z
     ## Suppress warning message
     data[,2] <-suppressWarnings(log(Z-t_0))
     data[,3] <- as.numeric(Z>=t_0)
     data[,4:(nc+3)] <- as.matrix(covariate)
     data[is.na(data[,2]),2] <- -10
+    data[data[,2]==-Inf,2] <- -10
     data[,(nc+4)] <- D
     data[n,(nc+4)] <- 1
     data <- as.data.frame(data) #head(data,20)
     colnames(data)[1:3] <- c("Z", "log(Z-t_0)", "I[Z>t_0]")
     covar <- paste("covariate",1:nc,sep = "")
     colnames(data)[4:(nc+3)] <- covar
-    colnames(data)[(nc+4):(nc+5)] <- c("delta","Weight")
+    colnames(data)[(nc+4)] <- c("delta")
+    # colnames(data)[(nc+4):(nc+5)] <- c("delta","Weight")
+    data = na.omit(data)
+    # data <- data[ complete.cases(data[ , c(4:3+nc)]), ]
+    n = nrow(data)
 
-    ## Rcpp Li's weight with jump weight
+    ## Rcpp IPCW with jump weight
     sv <- survfit(Surv(data[,1], 1 - data[,(nc+4)]) ~ 1)
-    W <- data[,(nc+4)] / sv$surv[findInterval(data[,1], sv$time)]*sv$surv[min(which(floor(sv$time)==(t_0)))]
+    if (t_0<=sv$time[1]){
+        ghatt_0 = 1
+    } else {
+        ghatt_0 = sv$surv[max(which(floor(sv$time)<(t_0)))]
+    }
+    W <- data[,(nc+4)] / sv$surv[findInterval(data[,1], sv$time)]*ghatt_0
     W[is.na(W)] <- max(W, na.rm = TRUE)
     data[,(nc+5)] <- W
+    colnames(data)[(nc+5)] <- c("weight")
 
-    ## Covariate setting (1 covariate)
-
+    # Covariate setting
     X <- as.matrix(cbind(c(rep(1,n)),data[,4:(nc+3)]))
     logT <- data[,2]
     I <- data[,3]
@@ -81,7 +92,7 @@ qrismb <- function(Z, covariate, D, t_0 = 0, Q = 0.5, ne = 100, init="rq", metho
     } else if (init == "one"){
         betastart <- c(1,rep(0,nc))
     } else {
-        betastart <- as.vector(rq.wfit(X,data[,2], tau=Q, weight=W)$coef)
+        betastart <- as.vector(rq.wfit(X, data[,2], tau=Q, weight=W)$coef)
     }
 
     if (method == "nonsmooth"){
@@ -101,11 +112,11 @@ qrismb <- function(Z, covariate, D, t_0 = 0, Q = 0.5, ne = 100, init="rq", metho
             for (j in 1:ne){
                 # generating perturbation variable
                 eta <- rexp(n,1)
-                if (all(D==rep(1,n))){
+                if (all(data$delta==rep(1,n))){
                     W_star <- rep(1,n)
                 } else {
-                    Gest <- ghat(Z,1-D,eta)
-                    W_star <- D / Gest$survp[findInterval(Z, Gest$deathtime)]*Gest$survp[min(which(floor(Gest$deathtime)==(t_0)))]
+                    Gest <- ghat(data$Z,1-data$delta,eta)
+                    W_star <- data$delta / Gest$survp[findInterval(data$Z, Gest$deathtime)]*Gest$survp[max(which(floor(Gest$deathtime)<(t_0)))]
                     W_star[is.na(W_star)] <- max(W_star, na.rm = TRUE)
                 }
                 rev_pseudo1 <- -apply(X*I*W_star*eta,2,sum)
@@ -142,11 +153,11 @@ qrismb <- function(Z, covariate, D, t_0 = 0, Q = 0.5, ne = 100, init="rq", metho
             for (j in 1:ne){
                 ## generating perturbation variable
                 E <- rexp(n,1)
-                if (D==rep(1,n)){
+                if (all(data$delta==rep(1,n))){
                     W_star <- rep(1,n)
                 } else {
-                    Gest <- ghat(Z,1-D,E)
-                    W_star <- D / Gest$survp[findInterval(Z, Gest$deathtime)]*Gest$survp[min(which(floor(Gest$deathtime)==(t_0)))]
+                    Gest <- ghat(data$Z,1-data$delta,E)
+                    W_star <- data$delta / Gest$survp[findInterval(data$Z, Gest$deathtime)]*Gest$survp[max(which(floor(Gest$deathtime)<(t_0)))]
                     W_star[is.na(W_star)] <- max(W_star, na.rm = TRUE)
                 }
                 result <- rev_isObj(coefficient, X, W_star, H, E, I, logT, Q)
--- inst/examples/ex_qrismb.R
+++ inst/examples/ex_qrismb.R
@@ -1,15 +1,72 @@
-data("retinopathy", package = 'survival')
-reti <- retinopathy
-reti_rev <- reti
-reti_rev$risk <- scales::rescale(reti$risk, to = c(0,1), from = c(0,12))
-reti_rev$type <- as.numeric(reti$type) - 1
-reti_rev$typextrt <- reti_rev$type * reti$trt
-Z <- reti_rev$futime
-nc <- 5
-covariate <- as.matrix(reti_rev[,c(9,4,5,6,10)])
-D <- reti_rev$status
-t_0 <- 1
-Q <- 0.25
+#### 1. simulation data ####
+# a single binary covariate with success probability 0.5.
+# We generate failure time, T, from a Weibull distribution.
+# Potential censoring time, C, from uniform (0,c) independently from T.
+# Given condition r=rho_0, k=2, exp(beta_0)=5, exp(beta_1)=2
+
+# 1.1 data generation function
+data.gen<-function(samplesize, censor){
+  sim=matrix(NA,samplesize,5)
+  colnames(sim) = c("T","C","Z","X","censored")
+  # Generate C_i
+  sim[,2] = runif(samplesize,0,censor)
+  # Covariates (Control=0, Treatment=1)
+  sim[,4] = rbinom(samplesize,size=1,p=0.5)
+  # Generate T_i (Given Condition r=rho_0, k=2, exp(beta_0)=5, exp(beta_1)=2))
+  unif = runif(n=samplesize ,min = 0,max = 1)
+  for (q in 1:samplesize){
+    if (sim[q,4]==0){
+      sim[q,1]={{-log(1-unif[q])}^(1/k)}/r.initial.0
+    } else {
+      sim[q,1]={{-log(1-unif[q])}^(1/k)}/r.initial.1
+    }
+  }
+  # Generate Y_i (min(T,C))
+  sim[,3] = apply(sim[,1:2], 1, FUN=min)
+  # Censoring indicator (Censored=0, Not censored=1)
+  sim[,5]=I(sim[,1]<sim[,2])
+  # Ordering
+  sim = sim[order(sim[,3]),]
+  n = nrow(sim)
+  sim = as.data.frame(sim)
+  return(sim)
+}
+
+# 1.2 Given value
+exp.beta.initial.0=5
+exp.beta.initial.1=10
+k=2
+r.initial.0=(log(10/5))^(1/k)/exp.beta.initial.0
+r.initial.1=(log(10/5))^(1/k)/exp.beta.initial.1
+# c value of unif(0,c)
+c=24.35
+a<-data.gen(200,c)
+Z=a[,3]
+covariate=as.matrix(a[,4])
+D=a[,5]
+t_0=1
+Q=0.5
+ne=200
+
+fit1 <- qrismb(Z, covariate, D, t_0, Q, ne, "rq", "smooth")
+fit2 <- qrismb(Z, covariate, D, t_0, Q, ne, "random", "nonsmooth")
+
+data(cancer, package="survival")
+lung_rev <- lung[,c(2,3,4,5,7,8,9)]
+# delta : censored = 0, death = 1
+lung_rev$status <- as.numeric(lung$status) - 1
+# covariate 1 : age (39~82)
+# covariate 2 : sex(male = 0, female = 1)
+lung_rev$sex <- as.numeric(lung$sex) - 1
+# covariate 3 : ph.karno: Karnofsky performance score (bad=0-good=100) rated by physician
+# covariate 4 : pat.karno: Karnofsky performance score as rated by patient
+# covariate 5 : meal.cal: Calories consumed at meals
+# covariate 6 : wt.loss: Weight loss in last six months (pounds)
+Z <- lung_rev$time
+covariate <- as.matrix(lung_rev[,c(3:6)])
+D <- lung_rev$status
+t_0 <- 5
+Q <- 0.5
 ne <- 200
 
 fit1 <- qrismb(Z, covariate, D, t_0, Q, ne, "rq", "smooth")
