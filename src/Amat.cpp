a8bdec3569d7e57e6a33b2e8c7ec81e55d12ff60
kyuhyunkim07@gmail.com <35562513+Kyuhyun07@users.noreply.github.com>
2021-11-01 15:20:24 +0900

    Delete E from Amat

--- src/Amat.cpp
+++ src/Amat.cpp
@@ -7,7 +7,7 @@ using namespace arma;
 //' @noRd
 // [[Rcpp::export]]
 arma::mat Amat(arma::vec b, arma::mat X, arma::vec W_star, arma::mat H,
-	       arma::vec E, arma::vec I,arma::vec logT, double Q) {
+	       arma::vec I,arma::vec logT, double Q) {
   arma::mat m1 = X % repmat(I, 1, X.n_cols) % repmat(W_star, 1, X.n_cols);
   arma::mat m2 =  (-X / repmat(sqrt(diagvec(X * H * X.t())), 1, X.n_cols)) %
     repmat(normpdf((X * b - logT) / sqrt(diagvec(X * H * X.t()))), 1, X.n_cols);
