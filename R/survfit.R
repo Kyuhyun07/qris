6bd7aa001a839287d03acbe3e617e6b4b5da5036
kyuhyunkim07@gmail.com <35562513+Kyuhyun07@users.noreply.github.com>
2021-11-05 11:36:38 +0900

    add survfit

--- R/survfit.R
+++ R/survfit.R
@@ -1,16 +1,10 @@
-#' \code{Surv} function imported from \code{survival}
 #' \code{survfit} function imported from \code{survival}
 #'
 #' This function is imported from the \code{survival} package. See
-#' \code{\link[survival]{Surv}}.
 #' \code{\link[survival]{survfit}}.
 #'
-#' @importFrom survival Surv
 #' @importFrom survival survfit
-#' @name export_Surv
 #' @name export_survfit
-#' @aliases Surv
 #' @aliases survfit
-#' @export Surv
 #' @export survfit
 NULL
