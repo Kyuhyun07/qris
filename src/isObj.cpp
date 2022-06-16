2a96788d5b62e64f19488e1a7d1c82eb3ee934f1
Steven Chiou <syhan.chiou@gmail.com>
2022-06-15 23:46:36 -0500

    checked old c codes

--- src/isObj.cpp
+++ src/isObj.cpp
@@ -8,13 +8,19 @@ using namespace arma;
 // [[Rcpp::export(rng = FALSE)]]
 arma::mat isObj(arma::vec b, arma::mat X, arma::vec W, arma::mat H,
 		arma::vec I, arma::vec logT, double Q) {
-  // arma::mat m1 = X % repmat(I, 1, X.n_cols);
-  // arma::mat m2 = normcdf((X * b - logT) / sqrt(diagvec(X * H * X.t()))) % W - Q;
   arma::mat m1 = X;
   m1.each_col() %= I;
   arma::mat m2 = normcdf((X * b - logT) / sqrt(sum(X % (X * H), 1))) % W - Q;
   return m1.t() * m2;
 }
+
+// old version
+// arma::mat isObj(arma::vec b, arma::mat X, arma::vec W, arma::mat H,
+// 		arma::vec I, arma::vec logT, double Q) {
+//   arma::mat m1 = X % repmat(I, 1, X.n_cols);
+//   arma::mat m2 = normcdf((X * b - logT) / sqrt(diagvec(X * H * X.t()))) % W - Q;
+//   return m1.t() * m2;
+// }
 
 //' @noRd
 // [[Rcpp::export(rng = FALSE)]]
--- src/rev_isObj.cpp
+++ src/rev_isObj.cpp
@@ -8,11 +8,17 @@ using namespace arma;
 // [[Rcpp::export(rng = FALSE)]]
 arma::mat rev_isObj(arma::vec b, arma::mat X, arma::vec W, arma::mat H,
 		    arma::vec E, arma::vec I,arma::vec logT, double Q) {
-  // arma::mat m1 = X % repmat(I % E, 1, X.n_cols);
-  // arma::mat m2 = normcdf((X * b - logT) / sqrt(diagvec(X * H * X.t()))) % W - Q;
   arma::mat m1 = X;
   m1.each_col() %= I % E;  
   // H is a symmetric
   arma::mat m2 = normcdf((X * b - logT) / sqrt(sum(X % (X * H), 1))) % W - Q;
   return m1.t() * m2 ;
 }
+
+// old version
+// arma::mat rev_isObj(arma::vec b, arma::mat X, arma::vec W, arma::mat H,
+// 		    arma::vec E, arma::vec I,arma::vec logT, double Q) {
+//   arma::mat m1 = X % repmat(I % E, 1, X.n_cols);
+//   arma::mat m2 = normcdf((X * b - logT) / sqrt(diagvec(X * H * X.t()))) % W - Q;
+//   return m1.t() * m2 ;
+// }
