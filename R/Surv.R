e9dc92d2d41ba18c98634ffe4ac2f8a5a9441ad3
kyuhyunkim07@gmail.com <35562513+Kyuhyun07@users.noreply.github.com>
2022-08-30 22:02:00 +0900

    Final submission to CRAN

--- R/Surv.R
+++ R/Surv.R
@@ -6,5 +6,12 @@
 #' @importFrom survival Surv
 #' @name export_Surv
 #' @aliases Surv
+#' @return An object of class \code{Surv}.  There are methods for \code{print}, \code{is.na}, and subscripting survival objects. 
+#' \code{Surv} objects are implemented as a matrix of 2 or 3 columns that has further attributes. 
+#' These include the type (left censored, right censored, counting process, etc.) and labels for the states for multi-state objects.
+#' Any attributes of the input arguments are also preserved in \code{inputAttributes}.  
+#' This may be useful for other packages that have attached further information to data items such as labels;
+#' none of the routines in the survival package make use of these values, however. 
+#' In the case of \code{is.Surv}, a logical value \code{TRUE} if \code{x} inherits from class \code{"Surv"}, otherwise an \code{FALSE}.
 #' @export Surv
 NULL
